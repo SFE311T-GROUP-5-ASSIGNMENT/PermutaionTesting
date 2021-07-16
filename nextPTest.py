f8f6c351198d86e7ebf2ef3346d516254f3aaf61
Meltin25 <87095987+Meltin25@users.noreply.github.com>
2021-07-16 18:53:21 +0200

    Update nextPTest.py

--- nextPTest.py
+++ nextPTest.py
@@ -6,10 +6,7 @@ import types
 class nextPTest(unittest.TestCase):
 
     # Various test cases
-    mylist =  [5, 6, 7]
-    single_element = [4]
-    list_of_lists = [ [1.1, 1.2], [2.1, 2.2], [3.1, 3.2] ]
-    multilist = [ 'a', 2, 3.3 ]
+    mylist =  [1, 2, 3]
 
 
     def generic_test(self, elements):
@@ -37,22 +34,6 @@ class nextPTest(unittest.TestCase):
     def test_numeric_list(self):
         """Test for a list of numbers"""
         self.generic_test(self.mylist)
-
-
-
-    def test_single_element(self):
-        """Test for a single element."""
-        self.generic_test(self.single_element)
-
-
-    def test_list_of_lists(self):
-        """Test for a list of lists."""
-        self.generic_test(self.list_of_lists)
-
-
-    def test_multilist(self):
-        """Test for list consisting of various elements."""
-        self.generic_test(self.multilist)
 
 
 if __name__ == '__main__':
