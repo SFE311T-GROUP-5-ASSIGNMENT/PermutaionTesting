1d67651a944c4a538f7ef0e6f9069ac650302f73
Meltin25 <87095987+Meltin25@users.noreply.github.com>
2021-07-16 21:43:42 +0200

    Update nextPTest.py

--- nextPTest.py
+++ nextPTest.py
@@ -1,40 +1,20 @@
-import itertools
-import unittest
-import types
-
-
-class nextPTest(unittest.TestCase):
-
-    # Various test cases
-    mylist =  [1, 2, 3]
-
-
-    def generic_test(self, elements):
-        """Compares itertools.permutations(elements) vs permutations(elements)."""
-
-        # Arrange
-        expected = list(itertools.permutations(elements))
-
-        # Act
-        actual = list(itertools.permutations(elements))
-
-        # Assert
-        self.assertEqual(expected, actual)
-
-
-    def test_is_generator(self):
-        """Test that object is actually an generator."""
-        # Act
-        actual = itertools.permutations(self.numeric_list)
-
-        # Assert
-        self.assertTrue( isinstance(actual, types.GeneratorType))
-
-
-    def test_numeric_list(self):
-        """Test for a list of numbers"""
-        self.generic_test(self.mylist)
-
-
+class nextTest(unittest.TestCase):
+    #test the output of the correct value
+    def test_next(self):
+        #list to test
+        myList = [1,2,3]
+        #function call to be tested
+        result = nextPermutations(myList)
+        #assert
+        self.assertEqual(result, [1, 3, 2])  
+    #test for the wrong output  
+    def test_next_not_equal(self):
+        #list to test
+        myList = [1,2,3]
+        #function call to be tested
+        result = nextPermutations(myList)
+        #assert
+        self.assertNotEqual(result, [1, 2, 3])
+        
 if __name__ == '__main__':
     unittest.main()
